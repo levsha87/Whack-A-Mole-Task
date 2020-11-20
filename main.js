646b6600d0210345aa92ece25dfaf200aa2f4955
Roma Levsha <rm.levsha@gmail.com>
2020-11-20 11:51:26 +0300

    feat: add function for clear locslStorage and level games

--- main.js
+++ main.js
@@ -3,63 +3,126 @@ const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
 const buttonStartGame = document.querySelector('#startGame');
 const gameLevel = document.querySelector('.levelValue');
-console.log(buttonStartGame);
+let currentLevel, currentScore;
+let minTime, maxTime;
+
 let lasthole;
 let timeUp = false;
 let score = 0;
 
 function randomTime(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
+  return Math.round(Math.random() * (max - min) + min);
 }
 
-function randomHole(holes){
-    const index = Math.floor(Math.random() * holes.length);
-    const hole = holes[index];
-    if (hole === lasthole){
-        return randomHole(holes);
-    }
-    lasthole = hole;
-    return hole;
+function randomHole(holes) {
+  const index = Math.floor(Math.random() * holes.length);
+  const hole = holes[index];
+  if (hole === lasthole) {
+    return randomHole(holes);
+  }
+  lasthole = hole;
+  return hole;
 }
 
-function appearHidMole () {
-    const time = randomTime(200, 2000);
-    const hole = randomHole(holes);
-    hole.classList.add('up');
-    setTimeout(() => {
-        hole.classList.remove('up');
-        if(!timeUp) appearHidMole();
-    }, time);
+function appearHidMole() {
+  const time = randomTime(minTime, maxTime);
+  const hole = randomHole(holes);
+  hole.classList.add('up');
+  setTimeout(() => {
+    hole.classList.remove('up');
+    if (!timeUp) appearHidMole();
+  }, time);
 }
 
-function startGame(){
-    scoreBoard.textContent = 0;
-    timeUp = false;
+function setInfoLevelScore() {
+  localStorage.clear();
+  localStorage.setItem('level', `${gameLevel.classList[1]}`);
+  localStorage.setItem('score', `${score}`);
+}
+
+function getInfoLevelScore() {
+  currentLevel = localStorage.getItem('level');
+  currentScore = localStorage.getItem('score');
+  if (currentLevel === 'easy' && currentScore <= 7) {
+    localStorage.clear();
     score = 0;
-    appearHidMole();
-    setTimeout(() => {
-        timeUp = true;
-        finishGame();
-    }, 10000);
+  }
+  if (currentLevel === 'middle' && currentScore <= 15) {
+    localStorage.clear();
+    score = 0;
+  }
+  if (currentLevel === 'hard') {
+    localStorage.clear();
+    score = 0;
+    gameLevel.classList.add('easy');
+  }
 }
 
-function startLevelMiddle(){
-    let currentLevel = localStorage.getItem('level');
-    let currentScore = localStorage.getItem('score');
+function startGame() {
+  getInfoLevelScore();
+  if (score === 0) {
+    startLevelEasy();
+  }
+  if (currentLevel === 'easy' && currentScore > 7) {
+    startLevelMiddle();
+  }
+  if (currentLevel === 'middle' && currentScore > 15) {
+    startLevelHard();
+  }
 }
 
-
-function finishGame(){
-    localStorage.setItem('level', `${gameLevel.classList[1]}`);
-    localStorage.setItem('score', `${score}`);
+function startLevelEasy() {
+  minTime = 400;
+  maxTime = 1800;
+  gameLevel.classList.remove('hard');
+  gameLevel.classList.remove('middle');
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  appearHidMole(minTime, maxTime);
+  setTimeout(() => {
+    timeUp = true;
+    setInfoLevelScore();
+  }, 12000);
 }
 
-function bonk (e){
-if(!e.isTrusted) return;
-score++;
-this.parentNode.classList.remove('up');
-scoreBoard.textContent = score;
+function startLevelMiddle() {
+  minTime = 200;
+  maxTime = 1600;
+  gameLevel.classList.remove('easy');
+  gameLevel.classList.add('middle');
+  scoreBoard.textContent = currentScore;
+  timeUp = false;
+  score = currentScore;
+  appearHidMole(minTime, maxTime);
+  setTimeout(() => {
+    timeUp = true;
+    setInfoLevelScore();
+  }, 15000);
 }
 
-moles.forEach(mole => mole.addEventListener('click', bonk));
+function startLevelHard() {
+  minTime = 200;
+  maxTime = 1000;
+  gameLevel.classList.remove('easy');
+  gameLevel.classList.remove('middle');
+  gameLevel.classList.add('hard');
+  scoreBoard.textContent = currentScore;
+  timeUp = false;
+  score = currentScore;
+  appearHidMole(minTime, maxTime);
+  setTimeout(() => {
+    timeUp = true;
+    setInfoLevelScore();
+  }, 20000);
+}
+
+function bonk(e) {
+  if (!e.isTrusted) return;
+  score++;
+  this.parentNode.classList.remove('up');
+  scoreBoard.textContent = score;
+}
+
+moles.forEach((mole) => mole.addEventListener('click', bonk));
 buttonStartGame.addEventListener('click', startGame);
