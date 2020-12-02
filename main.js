f3df6a1d0af37214323658e3ae03a1bbc10412e9
Roma Levsha <rm.levsha@gmail.com>
2020-12-02 18:02:29 +0300

    fix:correct  function startLevelEasy

--- main.js
+++ main.js
@@ -20,19 +20,12 @@ state = {
 
 
 
-window.addEventListener('load', setInfoLevelScore(gameLevel, state.score));
-buttonStartGame.addEventListener('click', () => startGame(gameLevel, state, holes));
+window.addEventListener('load', setInfoLevelScore(gameLevel, state));
+buttonStartGame.addEventListener('click', () => startGame(gameLevel, state, holes, scoreBoard));
 game.addEventListener('click', (e) => {
   if (e.target !== e.target.closest('.mole')) return;
   hit(e, scoreBoard, state);
 });
-
-
-/* showResult(gameLevel, state.score);
-toggleMiddleLevel(gameLevel, state.score);
-toggleHardLevel(gameLevel, state.score);
-getInfoLevelScore(gameLevel, state.score); */
-
 
 }
 
@@ -57,6 +50,7 @@ function randomHole(holes) {
 }
 
 function appearHidMole(state, holes) {
+  console.log(state.minTime, state.maxTime, state.timeUp);
   const time = randomTime(state.minTime, state.maxTime);
   const hole = randomHole(holes, state.lasthole);
 
@@ -68,21 +62,20 @@ function appearHidMole(state, holes) {
   }, time);
 }
 
-function startGame(gameLevel, state, holes) {
+function startGame(gameLevel, state, holes, scoreBoard) {
   randomHole(holes);
-  getInfoLevelScore(gameLevel, state.score);
-  appearHidMole(state, holes);
+  getInfoLevelScore(state.currentLevel, state.currentScore);
 
-  if (state.score <= 7) {
-    startLevelEasy(gameLevel, state);
+  if (state.currentScore <= 7) {
+    startLevelEasy(gameLevel, state, scoreBoard, holes);
   }
 
   if (state.currentLevel === 'easy' && state.score > 7) {
-    startLevelMiddle(gameLevel, state);
+    startLevelMiddle(gameLevel, state, scoreBoard);
   }
 
   if (state.currentLevel === 'middle' && state.score > 15) {
-    startLevelHard(gameLevel, state);
+    startLevelHard(gameLevel, state, scoreBoard);
   }
 }
 
@@ -100,58 +93,58 @@ function toggleHardLevel(gameLevel, state) {
   }
 }
 
-function startLevelEasy(state, gameLevel, scoreBoard) {
-  state.minTime = state.minTime + 200;
-  state.maxTime -= 200;
-
+function startLevelEasy(gameLevel, state, scoreBoard, holes) {
+  
+  if (state.currentLevel!==('easy')){
   gameLevel.classList.remove('hard');
   gameLevel.classList.remove('middle');
   gameLevel.classList.add('easy');
-
+  }
   scoreBoard.textContent = 0;
   state.timeUp = false;
   state.score = 0;
 
-  appearHidMole(state);
+  appearHidMole(state, holes);
 
   setTimeout(() => {
+    console.log('через 12 с');
     state.timeUp = true;
-    setInfoLevelScore(gameLevel, state.score);
-    showResult();
-    toggleMiddleLevel();
+    setInfoLevelScore(gameLevel, state);
+    showResult(gameLevel, state);
+    toggleMiddleLevel(gameLevel, state);
   }, 12000);
 }
 
-function startLevelMiddle(state, gameLevel, scoreBoard) {
+function startLevelMiddle(state, gameLevel, scoreBoard, holes) {
   state.maxTime -= 400;
   gameLevel.classList.add('middle');
   scoreBoard.textContent = currentScore;
   state.timeUp = false;
   state.score = state.currentScore;
 
-  appearHidMole(minTime, maxTime);
+  appearHidMole(state, holes);
 
   setTimeout(() => {
     state.timeUp = true;
-    setInfoLevelScore(gameLevel, state.score);
-    showResult();
-    toggleHardLevel();
+    setInfoLevelScore(gameLevel, state);
+    showResult(gameLevel, state);
+    toggleMiddleLevel(gameLevel, state);
   }, 15000);
 }
 
-function startLevelHard(state, gameLevel, scoreBoard) {
+function startLevelHard(state, gameLevel, scoreBoard, holes) {
   state.maxTime -= 1000;
   gameLevel.classList.add('hard');
   scoreBoard.textContent = state.currentScore;
   state.timeUp = false;
   state.score = state.currentScore;
 
-  appearHidMole(state);
+  appearHidMole(state, holes);
 
   setTimeout(() => {
     state.timeUp = true;
-    setInfoLevelScore(gameLevel, state.score);
-    showResult();
+    setInfoLevelScore(gameLevel, state);
+    showResult(gameLevel, state);
   }, 20000);
 }
 
@@ -201,13 +194,13 @@ function showResult(gameLevel, state) {
 
   if (state.score <= 7) {
     alert('You lose! Try again!');
-  } else {
-    alert('You lose! Try again!');
-  }
-
-  /* if (score > 7 && score <= 15 && `${gameLevel.classList[1]}` === 'middle') {
+  } /* else {
     alert('You lose! Try again!');
   } */
+
+  if (score > 7 && score <= 15 && `${gameLevel.classList[1]}` === 'middle') {
+    alert('You lose! Try again!');
+  }
 
   if (`${gameLevel.classList[1]}` === 'hard') {
     alert(`Great! You win! Your score: ${state.score}`);
