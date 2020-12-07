dd55e12a136ed9705071bd96a1bb53ae6abd88b7
Roma Levsha <rm.levsha@gmail.com>
2020-12-07 21:21:11 +0300

    Feat: add function showResult, add state property minTime, maxTime

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-const MIN_MOLE_JUMP_IN_DELAY = 200;
-const MAX_MOLE_JUMP_IN_DELAY = 2000;
+const MIN_MOLE_JUMP_IN_DELAY = 400;
+const MAX_MOLE_JUMP_IN_DELAY = 1600;
 
-const GAME_DURATION = 10000;
+const GAME_DURATION = 20000;
 
 const LEVELS = {
   easy: {
-    name: 'easy', 
+    name: 'easy',
     lowThreshold: 0,
   },
   middle: {
     name: 'middle',
-    lowThreshold: 5,
+    lowThreshold: 12,
   },
   hard: {
     name: 'hard',
-    lowThreshold: 10,
-  }
+    lowThreshold: 30,
+  },
 };
 
 function initWhackMoleGame() {
@@ -31,6 +31,8 @@ function initWhackMoleGame() {
     lasthole: '',
     timeUp: false,
     score: 0,
+    minTime: MIN_MOLE_JUMP_IN_DELAY,
+    maxTime: MAX_MOLE_JUMP_IN_DELAY,
   };
 
   window.addEventListener('load', () => {
@@ -50,6 +52,16 @@ function initWhackMoleGame() {
 
 initWhackMoleGame();
 
+function setInfoLevelScore(state) {
+  localStorage.setItem('level', `${state.currentLevel}`);
+  localStorage.setItem('score', `${state.score}`);
+}
+
+function getInfoLevelScore() {
+  state.currentLevel = localStorage.getItem('level');
+  state.currentScore = localStorage.getItem('score');
+}
+
 function getRandomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
@@ -67,7 +79,7 @@ function getRandomHole(holes) {
 }
 
 function appearHiddenMole(state, holes) {
-  const time = getRandomTime(MIN_MOLE_JUMP_IN_DELAY, MAX_MOLE_JUMP_IN_DELAY);
+  const time = getRandomTime(state.minTime, state.maxTime);
   const hole = getRandomHole(holes, state.lasthole);
 
   hole.classList.add('up');
@@ -78,26 +90,12 @@ function appearHiddenMole(state, holes) {
   }, time);
 }
 
-function toggleLevel(gameLevel, state) {
-  getInfoLevelScore(state.currentLevel, state.currentScore);
-
-  if (state.currentLevel === 'easy' && state.currentScore > LEVELS.middle.lowThreshold) {
-    gameLevel.classList.remove(LEVELS.easy.name);
-    gameLevel.classList.add(LEVELS.middle.name);
-    state.currentLevel = LEVELS.middle.name; 
-  } else if (state.currentLevel === 'middle' && state.currentScore > LEVELS.hard.lowThreshold) {
-    gameLevel.classList.remove(LEVELS.middle.name);
-    gameLevel.classList.add(LEVELS.hard.name);
-    state.currentLevel = LEVELS.hard.name;    
-  } else {
-    gameLevel.classList.remove(LEVELS.middle.name);
-    gameLevel.classList.remove(LEVELS.hard.name);
-    gameLevel.classList.add(LEVELS.easy.name); 
-    state.currentLevel = LEVELS.easy.name;
-    state.score = 0;
-    state.currentScore = 0;
-  }
-} 
+function hit(e, scoreBoard, state) {
+  if (!e.isTrusted) return;
+  state.score++;
+  e.target.parentNode.classList.remove('up');
+  scoreBoard.textContent = state.score;
+}
 
 function startGame(state, scoreBoard, holes, gameLevel) {
   toggleLevel(gameLevel, state);
@@ -115,39 +113,65 @@ function startGame(state, scoreBoard, holes, gameLevel) {
   }, GAME_DURATION);
 }
 
-function hit(e, scoreBoard, state) {
-  if (!e.isTrusted) return;
-  state.score++;
-  e.target.parentNode.classList.remove('up');
-  scoreBoard.textContent = state.score;
-}
+function toggleLevel(gameLevel, state) {
+  getInfoLevelScore(state.currentLevel, state.currentScore);
 
-function setInfoLevelScore(state) {
-  localStorage.setItem('level', `${state.currentLevel}`);
-  localStorage.setItem('score', `${state.score}`);
-}
-
-function getInfoLevelScore() {
-  state.currentLevel = localStorage.getItem('level');
-  state.currentScore = localStorage.getItem('score');
+  if (
+    state.currentLevel === 'easy' &&
+    state.currentScore > LEVELS.middle.lowThreshold
+  ) {
+    gameLevel.classList.remove(LEVELS.easy.name);
+    gameLevel.classList.add(LEVELS.middle.name);
+    state.currentLevel = LEVELS.middle.name;
+    state.minTime = MIN_MOLE_JUMP_IN_DELAY - 100;
+    state.maxTime = MAX_MOLE_JUMP_IN_DELAY - 400;
+  } else if (
+    state.currentLevel === 'middle' &&
+    state.currentScore > LEVELS.hard.lowThreshold
+  ) {
+    gameLevel.classList.remove(LEVELS.middle.name);
+    gameLevel.classList.add(LEVELS.hard.name);
+    state.currentLevel = LEVELS.hard.name;
+    state.maxTime = MAX_MOLE_JUMP_IN_DELAY - 800;
+  } else {
+    gameLevel.classList.remove(LEVELS.middle.name);
+    gameLevel.classList.remove(LEVELS.hard.name);
+    gameLevel.classList.add(LEVELS.easy.name);
+    state.currentLevel = LEVELS.easy.name;
+    state.score = 0;
+    state.currentScore = 0;
+    state.minTime = MIN_MOLE_JUMP_IN_DELAY;
+    state.maxTime = MAX_MOLE_JUMP_IN_DELAY;
+  }
 }
 
 function showResult(state) {
-  alert(`Level: ${state.currentLevel}\n Score: ${state.score}`);
-
-  if (state.score <= state.levelUpMiddle) {
-    alert('You lose! Try again!');
-  }
-
+  getInfoLevelScore();
   if (
-    state.score > state.levelUpMiddle &&
-    state.score <= state.LevelUpHard &&
-    `${state.currentLevel}` === 'middle'
+    (state.currentLevel === 'easy' &&
+      state.currentScore > LEVELS.middle.lowThreshold) ||
+    (state.currentLevel === 'middle' &&
+      state.currentScore > LEVELS.hard.lowThreshold)
   ) {
-    alert('You lose! Try again!');
+    showPositiveResult(state);
+  } else if (state.currentLevel === 'hard') {
+    showFinallyResult(state);
+  } else {
+    showNegativeResult(state);
   }
+}
 
-  if (`${state.currentLevel}` === 'hard') {
-    alert(`Great! You win! Your score: ${state.score}`);
-  }
+function showPositiveResult(state) {
+  alert(`Level: ${state.currentLevel}\n Score: ${state.score}`);
+  alert('Good job! Go on!');
+}
+
+function showNegativeResult(state) {
+  alert(`Level: ${state.currentLevel}\n Score: ${state.score}`);
+  alert('You lose! Try again!');
+}
+
+function showFinallyResult(state) {
+  alert(`Level: ${state.currentLevel}\n Score: ${state.score}`);
+  alert(`Great! You win! Your score: ${state.score}`);
 }
